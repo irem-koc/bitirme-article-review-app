324f37154892b115ef2809f55e66cd985780e0a4
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-05-14 19:13:36 +0300

    feat: add onclick to next button component

--- fe/src/components/molecules/NextButton/NextButton.tsx
+++ fe/src/components/molecules/NextButton/NextButton.tsx
@@ -4,13 +4,17 @@ import { Link } from "react-router-dom";
 type Props = {
   content: string;
   to: string;
+  onclick: () => void;
 };
 
 const NextButton = (props: Props) => {
   const { content, to } = props;
   return (
     <Link to={to}>
-      <button className="flex items-center rounded-lg p-2 text-white bg-indigo-600 ">
+      <button
+        onClick={() => onclick()}
+        className="flex items-center rounded-lg p-2 text-white bg-indigo-600 "
+      >
         {content}
         <span className="pl-1">
           <GrFormNextLink />
