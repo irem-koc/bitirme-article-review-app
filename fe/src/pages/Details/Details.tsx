fda63e742e5787246c8d7ea197278149a822b8dd
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-05-14 19:16:03 +0300

    feat: create details page

--- fe/src/pages/Details/Details.tsx
+++ fe/src/pages/Details/Details.tsx
@@ -1,8 +1,34 @@
+import getAllReviews from "@services/getAllReviews";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
 type Props = {};
 
 const Details = (props: Props) => {
+  const [tasks, setTasks] = useState();
+  const location = useLocation();
+
+  const [errorText, setErrorText] = useState<string | undefined>();
+  const handleLogin = async () => {
+    try {
+      const res = await getAllReviews();
+      setTasks(res);
+    } catch (error) {
+      setErrorText(error.message);
+    }
+  };
+  useEffect(() => {
+    handleLogin();
+  }, [location.pathname]);
   const {} = props;
-  return <div>Details</div>;
+  return (
+    <div>
+      {errorText ? <p className="error">{errorText}</p> : null}
+      {tasks?.map((task) => (
+        <div>{task.title}</div>
+      ))}
+    </div>
+  );
 };
 
 export default Details;
