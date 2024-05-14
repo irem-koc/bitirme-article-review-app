ee54b555e1bd72429a8d1e43613659c69219ed3a
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-05-14 20:12:19 +0300

    feat: revised thisrd step, add overallAssesment to review entity

--- fe/src/components/organisms/ThirdStep/ThirdStep.tsx
+++ fe/src/components/organisms/ThirdStep/ThirdStep.tsx
@@ -1,9 +1,37 @@
-import NextButton from "@molecules/NextButton/NextButton";
+import { Context } from "@context/Context";
+import { useContext, useEffect, useState } from "react";
+import { GrFormNextLink } from "react-icons/gr";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const ThirdStep = (props: Props) => {
   const {} = props;
+  const { task, setTask } = useContext(Context);
+  const navigate = useNavigate();
+  const [errorText, setErrorText] = useState<string | undefined>();
+  const [selectedValue, setSelectedValue] = useState<string | undefined>(
+    task.overallAssessment
+  );
+
+  const handleRadioChange = (value: string) => {
+    setSelectedValue(value);
+  };
+
+  useEffect(() => {
+    if (selectedValue !== task.overallAssessment) {
+      selectedValue && setTask({ ...task, overallAssessment: selectedValue });
+    }
+  }, [selectedValue]);
+
+  const handleNext = () => {
+    if (selectedValue) {
+      navigate("/review/4");
+    } else {
+      setErrorText("Please select an option.");
+    }
+  };
+
   return (
     <div>
       <div className="flex flex-col items-center mt-4">
@@ -21,8 +49,9 @@ const ThirdStep = (props: Props) => {
                 <input
                   type="radio"
                   id="inadequate"
-                  value={1}
-                  label="Inadequate"
+                  value="1"
+                  checked={selectedValue === "1"}
+                  onChange={() => handleRadioChange("1")}
                 />
                 <label htmlFor="inadequate">1</label>
               </td>
@@ -32,7 +61,13 @@ const ThirdStep = (props: Props) => {
             </tr>
             <tr>
               <td className="border p-2">
-                <input type="radio" id="adequate" value={3} label="Adequate" />
+                <input
+                  type="radio"
+                  id="adequate"
+                  value="3"
+                  checked={selectedValue === "3"}
+                  onChange={() => handleRadioChange("3")}
+                />
                 <label htmlFor="adequate">3</label>
               </td>
               <td className="border p-2">
@@ -41,7 +76,13 @@ const ThirdStep = (props: Props) => {
             </tr>
             <tr>
               <td className="border p-2">
-                <input type="radio" id="good" value={5} label="Good" />
+                <input
+                  type="radio"
+                  id="good"
+                  value="5"
+                  checked={selectedValue === "5"}
+                  onChange={() => handleRadioChange("5")}
+                />
                 <label htmlFor="good">5</label>
               </td>
               <td className="border p-2">
@@ -54,8 +95,9 @@ const ThirdStep = (props: Props) => {
                 <input
                   type="radio"
                   id="excellent"
-                  value={1}
-                  label="Excellent"
+                  value="7"
+                  checked={selectedValue === "7"}
+                  onChange={() => handleRadioChange("7")}
                 />
                 <label htmlFor="excellent">7</label>
               </td>
@@ -63,7 +105,13 @@ const ThirdStep = (props: Props) => {
             </tr>
             <tr>
               <td className="border p-2">
-                <input type="radio" id="perfect" value={9} label="Perfect" />
+                <input
+                  type="radio"
+                  id="perfect"
+                  value="9"
+                  checked={selectedValue === "9"}
+                  onChange={() => handleRadioChange("9")}
+                />
                 <label htmlFor="perfect">9</label>
               </td>
               <td className="border p-2">
@@ -73,8 +121,25 @@ const ThirdStep = (props: Props) => {
           </tbody>
         </table>
       </div>
+      {errorText && (
+        <p className="flex items-center justify-end w-3/6 mx-auto my-6 text-red-500 mt-4">
+          {errorText}
+        </p>
+      )}
       <div className="flex items-center justify-end w-3/6 mx-auto my-6">
-        <NextButton content="Next" to="/review/4" />
+        <button
+          type="submit"
+          disabled={!selectedValue}
+          onClick={handleNext}
+          className={`flex items-center rounded-lg p-2 text-white ${
+            !selectedValue ? "bg-gray-400" : "bg-indigo-600"
+          }`}
+        >
+          Next
+          <span className="pl-1">
+            <GrFormNextLink />
+          </span>
+        </button>
       </div>
     </div>
   );
