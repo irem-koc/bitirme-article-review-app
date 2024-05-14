ca9627acb407ec566894a1b6f182d6aa894f5d07
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-05-14 19:13:14 +0300

    feat: add validation, state management to first and second review steps

--- fe/src/components/organisms/FirstStep/FirstStep.tsx
+++ fe/src/components/organisms/FirstStep/FirstStep.tsx
@@ -1,49 +1,75 @@
 import Label from "@atoms/Label/Label";
-import NextButton from "@molecules/NextButton/NextButton";
+import { Context } from "@context/Context";
+import { useContext, useEffect, useState } from "react";
+import { GrFormNextLink } from "react-icons/gr";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
 
 const FirstStep = (props: Props) => {
   const {} = props;
+  const { task, setTask } = useContext(Context);
+  const navigate = useNavigate();
+  const [errorText, setErrorText] = useState<string | undefined>();
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  useEffect(() => {
+    setIsDisabled(!task.title || task.title.trim().length === 0);
+  }, [task.title]);
+
+  const handleChange = (e) => {
+    setErrorText(undefined);
+    const { name, value } = e.target;
+    setTask({ ...task, [name]: value });
+  };
+
+  const handleNext = (e) => {
+    e.preventDefault();
+    if (task.title && task.title.trim().length > 0) {
+      navigate("/review/2");
+    } else {
+      setErrorText("Title cannot be empty!");
+    }
+  };
+
   return (
     <div className="flex flex-col justify-center items-center mt-4">
-      <div className="items-start w-1/2 mb-5">
-        <Label
-          content={"Paper ID"}
-          htmlFor={"paperId"}
-          style={["text-sm font-medium leading-6 text-gray-900"]}
-        />
-        <div className="mt-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 ">
-          <input
-            type="text"
-            name="paperId"
-            id="paperId"
-            autoComplete="paperId"
-            className="w-full  outline-none flex-1 border-0 bg-transparent  p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
-            placeholder="ex. 25"
+      <form className="w-1/2" onSubmit={handleNext}>
+        <div className="items-start mb-5">
+          <Label
+            content="Title"
+            htmlFor="title"
+            style={["text-sm font-medium leading-6 text-gray-900"]}
           />
+          <div className="mt-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+            <input
+              onChange={handleChange}
+              value={task.title}
+              type="text"
+              id="title"
+              name="title"
+              autoComplete="title"
+              className="block outline-none flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 sm:max-w-md"
+              placeholder="Enter title"
+            />
+          </div>
         </div>
-      </div>
-      <div className="items-start w-1/2 mb-5">
-        <Label
-          content={"Title"}
-          htmlFor={"title"}
-          style={["text-sm font-medium leading-6 text-gray-900"]}
-        />
-        <div className="mt-2 rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 ">
-          <input
-            type="text"
-            name="title"
-            id="title"
-            autoComplete="title"
-            className="block outline-none flex-1 border-0 bg-transparent p-2 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 sm:max-w-md"
-            placeholder="janesmith"
-          />
+        {errorText && <p className="error text-red-500">{errorText}</p>}
+        <div className="flex items-end justify-end">
+          <button
+            type="submit"
+            disabled={isDisabled}
+            className={`flex items-center rounded-lg p-2 text-white ${
+              isDisabled ? "bg-gray-400" : "bg-indigo-600"
+            }`}
+          >
+            Next
+            <span className="pl-1">
+              <GrFormNextLink />
+            </span>
+          </button>
         </div>
-      </div>
-      <div className="flex items-end justify-end w-1/2">
-        <NextButton content={"Next"} to="/review/2" />
-      </div>
+      </form>
     </div>
   );
 };
--- fe/src/components/organisms/SecondStep/SecondStep.tsx
+++ fe/src/components/organisms/SecondStep/SecondStep.tsx
@@ -1,35 +1,64 @@
-import NextButton from "@molecules/NextButton/NextButton";
-import { useState } from "react";
+import { Context } from "@context/Context";
+import { useContext, useEffect, useState } from "react";
+import { GrFormNextLink } from "react-icons/gr";
+import { useNavigate } from "react-router-dom";
 
 type Props = {};
+
 const RadioOption = ({ id, name, value, label, checked, onChange }) => (
-  <input
-    type="radio"
-    id={id}
-    name={name}
-    value={value}
-    checked={checked}
-    onChange={onChange}
-  />
+  <label className="flex items-center space-x-2">
+    <input
+      type="radio"
+      id={id}
+      name={name}
+      value={value}
+      checked={checked}
+      onChange={onChange}
+      className="form-radio"
+    />
+    <span>{label}</span>
+  </label>
 );
+
 const SecondStep = (props: Props) => {
   const {} = props;
+  const { task, setTask } = useContext(Context);
+  const navigate = useNavigate();
+  const [errorText, setErrorText] = useState<string | undefined>();
   const data = [
     "A. Originality, novelty and significance of results (weight 35%)?*",
-    "B.	Technical quality of the work (weight 25%)?*",
-    "C.	Comprehensibility and presentation of the paper (weight 20%)?*",
-    "D.	What is the overall impression given by the paper (weight 20%)?*",
+    "B. Technical quality of the work (weight 25%)?*",
+    "C. Comprehensibility and presentation of the paper (weight 20%)?*",
+    "D. What is the overall impression given by the paper (weight 20%)?*",
   ];
   const [selectedValues, setSelectedValues] = useState(
     Array(data.length).fill("")
   );
-  console.log(selectedValues);
+  useEffect(() => {
+    // Kullanıcının bir sonraki adıma geçtiğinde görev durumundaki değerleri selectedValues'e atayın
+    setSelectedValues(task.scores);
+  }, []);
+
+  useEffect(() => {
+    const allAnswered = selectedValues.every((value) => value !== "");
+    setErrorText(allAnswered ? undefined : "All questions must be answered.");
+  }, [selectedValues]);
 
   const handleRadioChange = (index, value) => {
     const newSelectedValues = [...selectedValues];
     newSelectedValues[index] = value;
     setSelectedValues(newSelectedValues);
+    setTask({ ...task, scores: newSelectedValues });
   };
+
+  const handleNext = () => {
+    if (selectedValues.every((value) => value !== "")) {
+      navigate("/review/3");
+    } else {
+      setErrorText("All questions must be answered.");
+    }
+  };
+
   return (
     <div>
       {data.map((question, index) => (
@@ -46,56 +75,52 @@ const SecondStep = (props: Props) => {
               <tr>
                 <td className="border p-2">
                   <RadioOption
-                    id="inadequate"
+                    id={`inadequate-${index}`}
                     name={`evaluation${index}`}
                     value="1"
-                    label="Inadequate"
+                    label="1"
                     checked={selectedValues[index] === "1"}
                     onChange={() => handleRadioChange(index, "1")}
                   />
-                  <label htmlFor="inadequate">1</label>
                 </td>
                 <td className="border p-2">Inadequate</td>
               </tr>
               <tr>
                 <td className="border p-2">
                   <RadioOption
-                    id="inadequate"
+                    id={`adequate-${index}`}
                     name={`evaluation${index}`}
-                    value="1"
-                    label="Inadequate"
+                    value="5"
+                    label="5"
                     checked={selectedValues[index] === "5"}
                     onChange={() => handleRadioChange(index, "5")}
                   />
-                  <label htmlFor="adequate">5</label>
                 </td>
                 <td className="border p-2">Adequate</td>
               </tr>
               <tr>
                 <td className="border p-2">
                   <RadioOption
-                    id="inadequate"
+                    id={`good-${index}`}
                     name={`evaluation${index}`}
-                    value="1"
-                    label="Inadequate"
+                    value="7"
+                    label="7"
                     checked={selectedValues[index] === "7"}
                     onChange={() => handleRadioChange(index, "7")}
                   />
-                  <label htmlFor="good">7</label>
                 </td>
                 <td className="border p-2">Good</td>
               </tr>
               <tr>
                 <td className="border p-2">
                   <RadioOption
-                    id="inadequate"
+                    id={`excellent-${index}`}
                     name={`evaluation${index}`}
-                    value="1"
-                    label="Inadequate"
+                    value="9"
+                    label="9"
                     checked={selectedValues[index] === "9"}
                     onChange={() => handleRadioChange(index, "9")}
                   />
-                  <label htmlFor="excellent">9</label>
                 </td>
                 <td className="border p-2">Excellent</td>
               </tr>
@@ -103,8 +128,27 @@ const SecondStep = (props: Props) => {
           </table>
         </div>
       ))}
+      {errorText && (
+        <p className="flex items-center justify-end w-3/6 mx-auto my-6 text-red-500 mt-4">
+          {errorText}
+        </p>
+      )}
       <div className="flex items-center justify-end w-3/6 mx-auto my-6">
-        <NextButton content={"Next"} to="/review/3" />
+        <button
+          type="submit"
+          disabled={selectedValues.some((value) => value === "")}
+          onClick={handleNext}
+          className={`flex items-center rounded-lg p-2 text-white ${
+            selectedValues.some((value) => value === "")
+              ? "bg-gray-400"
+              : "bg-indigo-600"
+          }`}
+        >
+          Next
+          <span className="pl-1">
+            <GrFormNextLink />
+          </span>
+        </button>
       </div>
     </div>
   );
