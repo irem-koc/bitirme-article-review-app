8d89ab0ca8cdf6a3d950bac6adb41d8f5b694142
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-05-15 12:09:06 +0300

    edit jwt manager expiration time

--- be/src/main/java/dev/tugba/taskapp/auth/config/concretes/JwtManager.java
+++ be/src/main/java/dev/tugba/taskapp/auth/config/concretes/JwtManager.java
@@ -27,7 +27,7 @@ public class JwtManager implements JwtService {
                 .subject(user.getUsername())
                 .claim("userId", user.getId())
                 .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000))
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60))
                 .signWith(getSigninKey())
                 .compact();
     }
